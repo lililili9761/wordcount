baf1f915228672ac722a3cb1969c538b6ea0f09b
lililili <tong9761@qq.com>
2018-03-21 00:45:12 +0800

    add test.bat

---
+++ bin/test.bat
@@ -0,0 +1,10 @@
+wc.exe -c output.txt
+wc.exe -w -l Filecount.java
+wc.exe -a output.txt
+wc.exe -w -e stopList.txt Filecount.java 
+wc.exe -s -w -l -a E:\wordc\bin\*.txt
+wc.exe -s -w -l -c -a E:\wordc\bin\*.txt -o output1.txt
+wc.exe -s -w -l -c -a -e stopList.txt E:\wordc\bin\*.txt -o output2.txt
+wc.exe -c output.txt -w result.txt -l stopList.txt
+wc.exe -c wrong.c 
+wc.exe -b output.txt
